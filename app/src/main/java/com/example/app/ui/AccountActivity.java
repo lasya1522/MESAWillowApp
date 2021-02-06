3fea53d7353b7fd53d5c6bbdf3abdfe3b5a4ca4b
lasya1522 <lasya1522@gmail.com>
2021-02-06 00:13:18 -0500

    started layout for log in screen

---
+++ app/src/main/java/com/example/app/ui/AccountActivity.java
@@ -0,0 +1,16 @@
+package com.example.app.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.app.R;
+
+public class AccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+    }
+}
