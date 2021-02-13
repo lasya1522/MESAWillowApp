02474dbe75d774287dd8e62840f653d0e26407aa
lasya1522 <lasya1522@gmail.com>
2021-02-12 23:33:00 -0500

    Login coded and should work-but doesn't display yet

--- app/src/main/java/com/example/app/AccountActivity.java
+++ app/src/main/java/com/example/app/AccountActivity.java
@@ -1,14 +1,59 @@
 package com.example.app;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AccountActivity extends AppCompatActivity {
+    EditText username;
+    EditText password;
+    Button logIn;
 
+//does not display when app is run yet
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+        username = findViewById(R.id.usernameText);
+        password = findViewById(R.id.passwordText);
+        logIn = findViewById(R.id.logInBtn);
+
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //set username as "Willow", Password as "test"
+                if(username.getText().equals("willow")){
+                    if(password.getText().toString().equals("test")){
+                        AlertDialog.Builder builder = new AlertDialog.Builder(
+                                AccountActivity.this);
+                        builder.setTitle("Login Succesful!");
+                        builder.setMessage("Welcome Back!");
+
+                        builder.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                        AlertDialog alert = builder.create();
+                        alert.show();
+                    }
+                    else{ //invalid toast message
+                        Toast.makeText(getApplicationContext(), "Invalid Username or Password",
+                                Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(AccountActivity.this, "", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+
+            }
+        });
     }
 }
