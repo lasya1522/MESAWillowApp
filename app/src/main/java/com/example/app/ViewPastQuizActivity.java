2fd98901b8fb337034c58eb9cab5033f9e8f4b91
Emily Davies <emily@daviesland.net>
2021-02-14 15:46:32 -0500

    added CardViews to ViewPastQuizActivity added message when you click on a quiz that does not exist

--- app/src/main/java/com/example/app/ViewPastQuizActivity.java
+++ app/src/main/java/com/example/app/ViewPastQuizActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -24,6 +25,14 @@ public class ViewPastQuizActivity extends AppCompatActivity {
     TextView tv_stressLevel;
     TextView tv_stressors;
     TextView tv_other;
+
+    CardView card_mood;
+    CardView card_sleep;
+    CardView card_productive;
+    CardView card_relax;
+    CardView card_exercise;
+    CardView card_stress;
+    CardView card_other;
 
     List<DailyQuiz> list;
     DailyQuiz dailyQuiz;
@@ -47,21 +56,40 @@ public class ViewPastQuizActivity extends AppCompatActivity {
         tv_stressors = findViewById(R.id.tv_stressors);
         tv_other = findViewById(R.id.tv_other);
 
+        card_mood = findViewById(R.id.past_quiz_mood_card);
+        card_sleep = findViewById(R.id.past_quiz_sleep_card);
+        card_relax = findViewById(R.id.past_quiz_relax_card);
+        card_productive = findViewById(R.id.past_quiz_productive_card);
+        card_exercise = findViewById(R.id.past_quiz_exercise_card);
+        card_stress = findViewById(R.id.past_quiz_stress_card);
+        card_other = findViewById(R.id.past_quiz_other_card);
+
         Intent intent = getIntent();
         String passedDate = intent.getStringExtra("date");
 
         DatabaseHelper databaseHelper = new DatabaseHelper(this);
         dailyQuiz = databaseHelper.getDailyQuiz(passedDate);
         tv_date.setText("Quiz results for " + passedDate);
-        tv_mood.setText("Mood: " + dailyQuiz.getMood());
-        tv_sleepTime.setText("Hours spent sleeping: " + dailyQuiz.getSleepTime().toString());
-        tv_sleepRating.setText("Sleep rating: " + dailyQuiz.getSleepRating());
-        tv_productiveTime.setText("Hours spent being productive: " + dailyQuiz.getProductiveTime().toString());
-        tv_relaxTime.setText("Hours spent relaxing: " + dailyQuiz.getRelaxTime().toString());
-        tv_exerciseTime.setText("Hours spend exercising: " + dailyQuiz.getExerciseTime().toString());
-        tv_stressLevel.setText("Stress levels: " + dailyQuiz.getStressLevel());
-        tv_stressors.setText("Stressors: " + dailyQuiz.getStressors());
-        tv_other.setText("Other notes: " + dailyQuiz.getOther());
+
+        if (dailyQuiz.getDate().equals("")){
+            tv_mood.setText("It looks like you didn't fill out a daily quiz on " + passedDate);
+            card_relax.setVisibility(View.GONE);
+            card_sleep.setVisibility(View.GONE);
+            card_productive.setVisibility(View.GONE);
+            card_stress.setVisibility(View.GONE);
+            card_other.setVisibility(View.GONE);
+            card_exercise.setVisibility(View.GONE);
+        } else {
+            tv_mood.setText("Mood: " + dailyQuiz.getMood());
+            tv_sleepTime.setText("Hours spent sleeping: " + dailyQuiz.getSleepTime().toString());
+            tv_sleepRating.setText("Sleep rating: " + dailyQuiz.getSleepRating());
+            tv_productiveTime.setText("Hours spent being productive: " + dailyQuiz.getProductiveTime().toString());
+            tv_relaxTime.setText("Hours spent relaxing: " + dailyQuiz.getRelaxTime().toString());
+            tv_exerciseTime.setText("Hours spend exercising: " + dailyQuiz.getExerciseTime().toString());
+            tv_stressLevel.setText("Stress levels: " + dailyQuiz.getStressLevel());
+            tv_stressors.setText("Stressors: " + dailyQuiz.getStressors());
+            tv_other.setText("Other notes: " + dailyQuiz.getOther());
+        }
 
 
     }
